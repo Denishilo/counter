eb22857bd3e6a15570e86da2e17a4e064ba27a10
Denishilo <denisanishenko@gmail.com>
2022-11-02 23:38:14 +0300

    completed task with Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,165 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useReducer} from 'react';
 import './App.css';
 import {Counter} from "./Components/Counter/Counter";
 import {Settings} from "./Components/Counter/Settings/Settings";
 import {Counter2} from "./Components/Counter2/Counter2";
-
+import {
+    changeMaxValueAC,
+    changeStartValueAC,
+    increaseValueAC, openClosedSettingsAC,
+    Reducer,
+    resetValueAC, setMaxValueFromLocalStorageAC, setStartValueFromLocalStorageAC,
+    setStartValueSettingsAC, setValueSettingsFromLocalStorageAC
+} from "./State/Reducer";
 
 function App() {
 
-    let [maxValue, setMaxValue] = useState<number>(0)
-    let [startValue, setStartValue] = useState<number>(0)
-    let [valueSetting, setValueSetting] = useState<number>(0)
-    let [changeInput, setChangeInput] = useState<boolean>(false)
-    let [isSettings, setSettings] = useState<boolean>(false)
+    const stateValues = {
+        maxValue : 1,
+        startValue:0,
+        valueSetting:0,
+        changeInput:false,
+        isSettings:false,
+    }
+
+    // let [maxValue, setMaxValue] = useState<number>(0)
+    // let [startValue, setStartValue] = useState<number>(0)
+    // let [valueSetting, setValueSetting] = useState<number>(0)
+    // let [changeInput, setChangeInput] = useState<boolean>(false)
+    // let [isSettings, setSettings] = useState<boolean>(false)
+
+    const[state, dispatch] = useReducer(Reducer,stateValues)
+
+    // useEffect(() => {
+    //     let maxItemStorage = localStorage.getItem(`maxValue`)
+    //     if (maxItemStorage) {
+    //         setMaxValue(JSON.parse(maxItemStorage))
+    //     }
+    //
+    //     let startItemStorage = localStorage.getItem('startValue')
+    //     if (startItemStorage) {
+    //         setValueSetting(JSON.parse(startItemStorage))
+    //     }
+    //
+    //     let currentItemStorage = localStorage.getItem('currentValue')
+    //     if (currentItemStorage) {
+    //         setStartValue(JSON.parse(currentItemStorage))
+    //     }
+    //
+    // }, [])
 
     useEffect(() => {
         let maxItemStorage = localStorage.getItem(`maxValue`)
         if (maxItemStorage) {
-            setMaxValue(JSON.parse(maxItemStorage))
+            let maxValue = JSON.parse(maxItemStorage)
+            dispatch(setMaxValueFromLocalStorageAC(maxValue))
         }
 
         let startItemStorage = localStorage.getItem('startValue')
         if (startItemStorage) {
-            setValueSetting(JSON.parse(startItemStorage))
+            let valueSettings = JSON.parse(startItemStorage)
+            dispatch(setValueSettingsFromLocalStorageAC(valueSettings))
         }
 
         let currentItemStorage = localStorage.getItem('currentValue')
         if (currentItemStorage) {
-            setStartValue(JSON.parse(currentItemStorage))
+            let startValue = JSON.parse(currentItemStorage)
+            dispatch(setStartValueFromLocalStorageAC(startValue))
         }
 
     }, [])
 
+    // const increaseValue = () => {
+    //     setStartValue(++startValue)
+    //     localStorage.setItem('currentValue', JSON.stringify(startValue));
+    // }
     const increaseValue = () => {
-        setStartValue(++startValue)
-        localStorage.setItem('currentValue', JSON.stringify(startValue));
+        dispatch(increaseValueAC())
+        localStorage.setItem('currentValue', JSON.stringify(state.startValue+1));
     }
+
+    // const resetValue = () => {
+    //     setStartValue(valueSetting)
+    // }
 
     const resetValue = () => {
-        setStartValue(valueSetting)
+        dispatch(resetValueAC())
     }
+
+    // const changeMaxValue = (num: number) => {
+    //     setMaxValue(num)
+    //     setChangeInput(true)
+    // }
 
     const changeMaxValue = (num: number) => {
-        setMaxValue(num)
-        setChangeInput(true)
+        dispatch(changeMaxValueAC(num))
     }
 
+    // const changeStartValue = (num: number) => {
+    //     setValueSetting(num)
+    //     setChangeInput(true)
+    // }
+
     const changeStartValue = (num: number) => {
-        setValueSetting(num)
-        setChangeInput(true)
+        dispatch(changeStartValueAC(num))
     }
 
     const setLocalStorage = () => {
-        localStorage.setItem('maxValue', JSON.stringify(maxValue));
-        localStorage.setItem('startValue', JSON.stringify(valueSetting))
+        localStorage.setItem('maxValue', JSON.stringify(state.maxValue));
+        localStorage.setItem('startValue', JSON.stringify(state.valueSetting))
     }
+
+    // const setStartValueSettings = () => {
+    //     setStartValue(valueSetting)
+    //     setLocalStorage();
+    //     setChangeInput(false)
+    // }
 
     const setStartValueSettings = () => {
-        setStartValue(valueSetting)
+        dispatch(setStartValueSettingsAC())
         setLocalStorage();
-        setChangeInput(false)
+
     }
 
+    // const openClosedSettings = () => {
+    //     if(!isSettings){
+    //         setSettings(!isSettings)
+    //     } else {
+    //         setStartValue(valueSetting)
+    //         setLocalStorage();
+    //         setChangeInput(false)
+    //         setSettings(!isSettings)
+    //     }
+    // }
+
     const openClosedSettings = () => {
-        if(!isSettings){
-            setSettings(!isSettings)
-        } else {
-            setStartValue(valueSetting)
-            setLocalStorage();
-            setChangeInput(false)
-            setSettings(!isSettings)
-        }
+        dispatch(openClosedSettingsAC())
+        setLocalStorage();
 
     }
 
     return (
-        <div className='wrapperApp'>
-            <Settings changeMaxValue={changeMaxValue} changeStartValue={changeStartValue} maxValue={maxValue}
-                      setStartValueSettings={setStartValueSettings} valueSetting={valueSetting}/>
-            <Counter startValue={startValue} increaseValue={increaseValue} resetValue={resetValue}
-                     maxValue={maxValue} valueSetting={valueSetting} changeInput={changeInput}/>
-            <Counter2 maxValue={maxValue} startValue={startValue} increaseValue={increaseValue} resetValue={resetValue}
-                      valueSetting={valueSetting} changeInput={changeInput} openClosedSettings={openClosedSettings} isSettings={isSettings}
-                      changeMaxValue={changeMaxValue} changeStartValue={changeStartValue}
-            />
-        </div>
+        // <div className='wrapperApp'>
+        //     <Settings changeMaxValue={changeMaxValue} changeStartValue={changeStartValue} maxValue={maxValue}
+        //               setStartValueSettings={setStartValueSettings} valueSetting={valueSetting}/>
+        //     <Counter startValue={startValue} increaseValue={increaseValue} resetValue={resetValue}
+        //              maxValue={maxValue} valueSetting={valueSetting} changeInput={changeInput}/>
+        //     <Counter2 maxValue={maxValue} startValue={startValue} increaseValue={increaseValue} resetValue={resetValue}
+        //               valueSetting={valueSetting} changeInput={changeInput} openClosedSettings={openClosedSettings} isSettings={isSettings}
+        //               changeMaxValue={changeMaxValue} changeStartValue={changeStartValue}
+        //     />
+        // </div>
+
+    <div className='wrapperApp'>
+        <Settings changeMaxValue={changeMaxValue} changeStartValue={changeStartValue} maxValue={state.maxValue}
+                  setStartValueSettings={setStartValueSettings} valueSetting={state.valueSetting}/>
+        <Counter startValue={state.startValue} increaseValue={increaseValue} resetValue={resetValue}
+                 maxValue={state.maxValue} valueSetting={state.valueSetting} changeInput={state.changeInput}/>
+        <Counter2 maxValue={state.maxValue} startValue={state.startValue} increaseValue={increaseValue} resetValue={resetValue}
+                  valueSetting={state.valueSetting} changeInput={state.changeInput} openClosedSettings={openClosedSettings} isSettings={state.isSettings}
+                  changeMaxValue={changeMaxValue} changeStartValue={changeStartValue}
+        />
+    </div>
     );
 }
 
---
+++ src/State/Reducer.ts
@@ -0,0 +1,166 @@
+type stateType = {
+    maxValue: number
+    startValue: number
+    valueSetting: number
+    changeInput: boolean
+    isSettings: boolean
+}
+type increaseValueType = {
+    type: 'INCREASE-VALUE'
+}
+type resetValueType = {
+    type: 'RESET-VALUE'
+}
+type changeMaxValueType = {
+    type: 'CHANGE-MAX-VALUE',
+    payload: {
+        num: number
+    }
+}
+type changeStartValueType = {
+    type: 'CHANGE-START-VALUE',
+    payload: {
+        num: number
+    }
+}
+type setStartValueSettingsType = {
+    type: 'SET-START-VALUE-SETTINGS'
+}
+type openClosedSettingsType = {
+    type: 'OPEN-CLOSED-SETTINGS'
+}
+type setMaxValueFromLocalStorageType = {
+    type: 'SET-MAX-VALUE-FROM-LOCAL-STORAGE',
+    payload: {
+        num: number
+    }
+}
+type setValueSettingsFromLocalStorageType = {
+    type: 'SET-VALUE-SETTINGS-FROM-LOCAL-STORAGE',
+    payload: {
+        num: number
+    }
+}
+
+type setStartValueFromLocalStorageType = {
+    type: 'SET-START-VALUE-FROM-LOCAL-STORAGE',
+    payload: {
+        num: number
+    }
+}
+type allActionType =
+    increaseValueType
+    | resetValueType
+    | changeMaxValueType
+    | changeStartValueType
+    | setStartValueSettingsType
+    | openClosedSettingsType
+    | setMaxValueFromLocalStorageType
+    | setValueSettingsFromLocalStorageType
+    | setStartValueFromLocalStorageType
+
+export const Reducer = (state: stateType, action: allActionType): stateType => {
+    switch (action.type) {
+        case "INCREASE-VALUE": {
+            return {...state, startValue: state.startValue++}
+        }
+        case "RESET-VALUE": {
+            return {...state, startValue: state.valueSetting}
+        }
+        case "CHANGE-MAX-VALUE": {
+            return {...state, maxValue: action.payload.num, changeInput: true}
+        }
+        case "CHANGE-START-VALUE": {
+            return {...state, valueSetting: action.payload.num, changeInput: true}
+        }
+        case "SET-START-VALUE-SETTINGS": {
+            return {...state, startValue: state.valueSetting, changeInput: false}
+        }
+        case "OPEN-CLOSED-SETTINGS": {
+            if (!state.isSettings) {
+                return {...state, isSettings: !state.isSettings}
+            }
+            return {...state, startValue: state.valueSetting, changeInput: false, isSettings: !state.isSettings}
+        }
+        case "SET-MAX-VALUE-FROM-LOCAL-STORAGE": {
+            return {...state, maxValue: action.payload.num}
+        }
+        case "SET-VALUE-SETTINGS-FROM-LOCAL-STORAGE": {
+            return {...state, valueSetting: action.payload.num}
+        }
+        case "SET-START-VALUE-FROM-LOCAL-STORAGE": {
+            return {...state, startValue: action.payload.num}
+        }
+
+        default:
+            throw new Error('I dont understand this type')
+    }
+}
+
+export const increaseValueAC = () => {
+    return {
+        type: 'INCREASE-VALUE'
+    } as const
+}
+
+export const resetValueAC = () => {
+    return {
+        type: 'RESET-VALUE'
+    } as const
+}
+
+export const changeMaxValueAC = (num: number) => {
+    return {
+        type: 'CHANGE-MAX-VALUE',
+        payload: {
+            num
+        }
+    } as const
+}
+
+export const changeStartValueAC = (num: number) => {
+    return {
+        type: 'CHANGE-START-VALUE',
+        payload: {
+            num
+        }
+    } as const
+}
+export const setStartValueSettingsAC = () => {
+    return {
+        type: 'SET-START-VALUE-SETTINGS',
+    } as const
+}
+
+export const openClosedSettingsAC = () => {
+    return {
+        type: 'OPEN-CLOSED-SETTINGS'
+    } as const
+}
+
+export const setMaxValueFromLocalStorageAC = (num: number) => {
+    return {
+        type: 'SET-MAX-VALUE-FROM-LOCAL-STORAGE',
+        payload: {
+            num
+        }
+    } as const
+}
+
+export const setValueSettingsFromLocalStorageAC = (num: number) => {
+    return {
+        type: 'SET-VALUE-SETTINGS-FROM-LOCAL-STORAGE',
+        payload: {
+            num
+        }
+    } as const
+}
+
+export const setStartValueFromLocalStorageAC = (num: number) => {
+    return {
+        type: 'SET-START-VALUE-FROM-LOCAL-STORAGE',
+        payload: {
+            num
+        }
+    } as const
+}
